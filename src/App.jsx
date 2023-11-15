5cc8391d37a28bc10c771ec186b84a1d461975af
Aryan Kumar <aryanajha256@gmail.com>
2023-11-16 00:47:56 +0530

    Major code update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpense from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { GlobalProvider } from "./context/GlobalState";
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <GlobalProvider>
+      <div className="max-w-2xl m-auto flex items-center justify-center">
+        <Header />
+        <div className="w-full p-4">
+          <Balance />
+          <IncomeExpense />
+          <TransactionList />
+          <AddTransaction />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </GlobalProvider>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1 className="text-2xl font-bold">Expense Tracker</h1>
+    </header>
+  );
+};
+
+export default Header;
